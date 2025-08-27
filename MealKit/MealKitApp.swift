67e6482405042db1254373d66d278e8ef33487e3
Yiwei04 <jefferywang812@gmail.com>
2025-08-27 16:06:54 +1000

    Initial Commit

---
+++ MealKit/MealKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealKitApp.swift
+//  MealKit
+//
+//  Created by Jeffery Wang on 27/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealKitTests/MealKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealKitTests.swift
+//  MealKitTests
+//
+//  Created by Jeffery Wang on 27/8/2025.
+//
+
+import Testing
+@testable import MealKit
+
+struct MealKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
